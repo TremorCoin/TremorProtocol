00397b0402e529eb9c028ba91f4930ba78e019b8
TremorCoin <tremorcoin@protonmail.com>
2018-01-29 19:34:42 -0500

    DNS Seeds Changed

--- tremorcoin/src/net.cpp
+++ tremorcoin/src/net.cpp
@@ -1173,10 +1173,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
+    {"tremorcoin.com", "dnsseed.tremorcoin.com"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
+    {"tremorcoin.com", "dnsseed.tremorcoin.com"},
     {NULL, NULL}
 };
 
